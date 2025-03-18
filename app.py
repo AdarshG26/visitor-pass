73c7f59be5c9a173ffeaecf478266d383eeb6d69
AdarshG26 <adarshg2612@gmail.com>
2025-03-18 21:38:03 +0530

    minor updates

--- app.py
+++ app.py
@@ -1,24 +1,45 @@
 from flask import Flask, render_template, request, redirect, url_for, flash
+from flask_login import LoginManager, UserMixin, login_user, login_required, logout_user
 from pymongo import MongoClient
 from flask_bcrypt import Bcrypt
+from bson import ObjectId       
 import os
 
 app = Flask(__name__)
 app.secret_key = "secret123"
+
+login_manager = LoginManager(app)
+login_manager.login_view = "login"
 bcrypt = Bcrypt(app)
 
 client = MongoClient(os.getenv('MONGO_URI'))
-# print(f"hello this is mongo uri: {client}")
 db = client["visitor_pass"]
+
+
+# User Model for login
+class User(UserMixin):
+    def __init__(self, user_id, username, email, password):
+        self.id = user_id
+        self.username = username
+        self.email = email
+        self.password = password
+
+
+@login_manager.user_loader
+def load_user(user_id):
+    try:
+        user_data = db.user.find_one({"_id": ObjectId(user_id)})
+        if user_data:
+            return User(str(user_data["_id"]), user_data["user_name"], user_data["email"], user_data["password"])
+    except:
+        return None
+    return None
+
 
 @app.route("/")
 def test():
     return redirect(url_for('login'))
 
-
-@app.route("/login")
-def login():
-    return render_template("login.html")
 
 @app.route("/register", methods=["GET", "POST"])
 def register():
@@ -50,9 +71,38 @@ def register():
     
     return render_template("register.html")
 
+
+@app.route("/login", methods=["GET", "POST"])
+def login():
+    if request.method == "POST":
+        email = request.form.get("email")
+        password = request.form.get("password")
+
+        user_data = db.user.find_one({"email": email})
+        if user_data and bcrypt.check_password_hash(user_data["password"], password):
+            user = User(str(user_data["_id"]), user_data["user_name"], user_data["email"], user_data["password"])
+            login_user(user)
+
+            flash("Login successful!", "success")
+            return redirect(url_for('dashboard'))
+        else:
+            flash("Invalid email or password", "danger")
+            
+    return render_template("login.html")
+
+
 @app.route("/dashboard")
-def dashboard():
+@login_required
+def dashboard():   
     return render_template("dashboard.html")
+
+
+@app.route("/logout")
+@login_required
+def logout():
+    logout_user()
+    flash("You have been logout!", "info")
+    return redirect(url_for("login"))
 
 
 if __name__ == "__main__":
