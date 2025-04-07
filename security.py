4072899311e0baeb54005b7c334519c208dcdd79
Gudiya <gudiya.elgoss@gmail.com>
2025-04-07 15:26:52 +0530

    show

--- security.py
+++ security.py
@@ -1,22 +1,30 @@
 from flask import Flask,render_template,request
 app = Flask(__name__)
 
-@app.route("/") 
-def index():
-    return "hello"
+# @app.route("/") 
+# def index():
+#     return "hello"
 
 @app.route("/security", methods=["POST","GET"]) 
 def security():
  return render_template("security.html")
 
 
-@app.route("/visitor", methods=["POST","GET"]) 
-def visitor ():
- return render_template("visitor.html")
+@app.route("/Overview", methods=["POST","GET"]) 
+def Overview ():
+ return render_template("Visitor_overview.html")
 
-@app.route("/active", methods=["POST","GET"]) 
-def active():
- return render_template("active.html")
+@app.route("/All", methods=["POST","GET"]) 
+def All():
+ return render_template("All_visitor.html")
+
+@app.route("/Visitor", methods=["POST","GET"]) 
+def Visitor():
+ return render_template("visitor.html")
+ 
+@app.route("/Rejected", methods=["POST","GET"]) 
+def Rejecte():
+ return render_template("rejected.html")
 
 
 if __name__== "__main__":
